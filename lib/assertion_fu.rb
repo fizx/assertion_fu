43392b2d1a745ac4be5321e44ec68c57569d194b
Kyle Maxwell <kyle@111.flood.pivotallabs.com>
2009-02-02 14:53:28 -0800

    version 0.1 file existance, etc

--- lib/assertion_fu.rb
+++ lib/assertion_fu.rb
@@ -15,5 +15,41 @@ module AssertionFu
     end
   end
   
+  # The contents of the file at the path should match the regex or
+  # include the string
+  def assert_file_includes(path, regex_or_string)
+    assert_file_exists(path)
+    content = File.read(path)
+    case regex_or_string
+    when Regexp
+      assert content =~ regex_or_string, 
+                     "#{path} doesn't match #{regex_or_string.inspect}"
+    else
+      assert content.include?(regex_or_string.to_s), 
+                     "#{path} doesn't include #{regex_or_string.inspect}"
+    end
+  end
+  
+  def assert_file_doesnt_include(path, regex_or_string)
+    assert_file_exists(path)
+    content = File.read(path)
+    case regex_or_string
+    when Regexp
+      assert !(content =~ regex_or_string), 
+                     "#{path} matches #{regex_or_string.inspect}"
+    else
+      assert !content.include?(regex_or_string.to_s), 
+                     "#{path} includes #{regex_or_string.inspect}"
+    end
+  end
+
+  def assert_file_exists(path)
+    assert File.exists?(path), "#{path} doesn't exist"
+  end
+  
+  def assert_file_doesnt_exist(path)
+    assert !File.exists?(path), "#{path} exists"
+  end
+  
   
 end
--- test/assertion_fu_test.rb
+++ test/assertion_fu_test.rb
@@ -4,6 +4,15 @@ require File.dirname(__FILE__) + "/../lib/assertion_fu"
 class AssertionFuTest < Test::Unit::TestCase #:nodoc:
   include AssertionFu
   
-  def test_hello_world
+  def test_file_exists
+    assert_file_exists(__FILE__)
+    assert_file_doesnt_exist(__FILE__ + "dfadsfa")
+  end
+  
+  def test_file_includes
+    assert_file_includes(__FILE__, "__FILE__")
+    assert_file_doesnt_include(__FILE__, "12341234" + "asdeasf")
+    assert_file_includes(__FILE__, /__FILE__/)
+    assert_file_doesnt_include(__FILE__, /dafaf{999}/)
   end
 end
